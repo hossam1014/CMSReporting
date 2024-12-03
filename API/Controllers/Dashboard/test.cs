39c4906ca2121b506bc821b489960a7bacb6697e
hossam11223 <hossam.mostafa@easacc.com>
2024-12-03 22:48:55 +0200

    making a seperate folder for both mobile app and dashboard

---
+++ API/Controllers/Dashboard/test.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers.Dashboard
+{
+    public class test
+    {
+        
+    }
+}
--- API/Controllers/MobileApp/test.cs
+++ API/Controllers/MobileApp/test.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Application.Dtos
+namespace API.Controllers.MobileApp
 {
-    public class BaseDto
+    public class test
     {
-        public int? Id { get; set; }
+        
     }
 }
